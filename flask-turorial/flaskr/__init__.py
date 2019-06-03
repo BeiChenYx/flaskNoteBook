6251d3ce548f3845bb455ede1517358bf7727837
yangxing <yangxing2297@163.com>
2019-06-03 14:05:23 +0800

    完成了Flask教程的flaskr项目

--- flask-turorial/flaskr/__init__.py
+++ flask-turorial/flaskr/__init__.py
@@ -55,7 +55,10 @@ def create_app(test_config=None):
 
     # 导入并注册蓝图
     from . import auth
+    from . import blog
     app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
 
